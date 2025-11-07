93f4b54e88b88c09599ca607ddb0dc9b4b62b5d1
kuromi916 <mads189645862@gmail.com>
2025-11-07 18:20:14 -0500

    initial Cat gif

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
+let Cat;
+
+function preload(){
+Cat = loadImage("Cat_gif.GIF");
+}
+
 function setup() 
 {
+     image (Cat, width/2, height/2);
 	createCanvas(windowWidth,windowHeight);
-    background("red");
+    background("gray");
+    //loadImage("Cat_gif.GIF");
 }
 
 function draw()
 {
- 
+ //image (Cat, width/2, height/2);
 }
