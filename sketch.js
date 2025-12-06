14b75fe95e8588ef04c9b0a7cf0ee8c06aac8e57
kuromi916 <mads189645862@gmail.com>
2025-12-05 19:20:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ text(countdown,width/2,height/2);
 
 //------------Level 1 --------------//
 fill("orange");
-rect1 = rect(310, 1400, 290, 50); //x, y , rectangle size
+rect1 = rect(310, 1400, 300, 50); //x, y , rectangle size
 
 // red rectangle base - negative space
 
