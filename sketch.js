8b47155a9d7c55d55e0480c574ecd86fa5e2f564
kuromi916 <mads189645862@gmail.com>
2025-12-05 21:34:38 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,11 +6,8 @@ let rect2;
 let hitX = 305;
 let hitWidth = 50;
 let twoMinutes = 120000;
+let touchStarted = false; 
 
-let isCurrentlyTouching = false;  // NEEDED - Track if screen is being touched
-let touchCounter = 0;             // NEEDED - Count total number of touches
-let touchStartTime = 0; // When current touch started (milliseconds)
-let touchDuration = 1; // How long current touch has been active (seconds)
 
 
 function preload(){
@@ -30,8 +27,7 @@ var hit = false;
 
 function draw(){
  
- let countdown = 120-floor(millis()/1000); 
- let touchDuration = (millis() - touchStartTime) / 1000;
+let countdown = 120-floor(millis()/1000); 
 
 background(255) ;
 image(BG,width/2,height/2,1100,2000);  
@@ -40,10 +36,9 @@ image(Cat,800,700,700,900);
 
 text(countdown,width/2,height/2);
 
-//------------Level 1 --------------//
+
 fill("orange");
 rect1 = rect(305, 1400, 310, 50); //x, y , rectangle size
-
 // red rectangle base - negative space
 
 fill("green");
@@ -59,26 +54,7 @@ rect2 = rect(hitX, 1400, hitWidth, 50);
   //line(50, y, x, y);
   fill("white");
   rect( x,y,4,50);
-//===================================//
 
-
-/*-----------Level 2 ---------------//
-fill("red");
-rect(170, 440, 120, 20);
-
-fill("green");
-rect(230,440, 10,20);
-
-let a = 60 * sin(frameCount * 0.05) + 230;
-let b = 440;
-
-  // Draw the oscillator.
-  //line(50, y, x, y);
-  fill("black");
-  rect( a,b,3,20);
-
- //=================================*/
-  
  
  // Use vectors as input:
     // const rect_start = createVector(200, 200);
@@ -94,29 +70,12 @@ let b = 440;
  
     hitX = random(305,1400);
     hitWidth = random(5,50);
-   // newTarget = false;
-    touchEnded===true;
-    //isCurrentlyTouching===false;
+   
     debug("Yes!");
   
-}
-
-    function touchStarted() 
-{
-  isCurrentlyTouching = true;
-  touchCounter = touchCounter + 1;
-  touchStartTime = millis();
-  //debug("Touch Count: " + touchCounter);  
-  return false;  // Prevents default behavior
-}
-
-function touchEnded() 
-{
-    isCurrentlyTouching = false;
-    // Output final touch duration to debug panel
-    //debug("Touch Duration: " + touchDuration.toFixed(2) + " seconds");
-    //debug("--- Touch Ended ---");
-    return false;  // Prevents default behavior
-}
+  if(touchStarted===true){
+    debug("yes!");
+    }
 
   }
+}
