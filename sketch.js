d8cef36ae208186c1310e7f3283d3e3e174e1bf7
kuromi916 <mads189645862@gmail.com>
2025-12-05 19:37:30 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,7 +45,7 @@ rect2 = rect(hitX, 1400, hitWidth, 50);
 
   // Calculate the coordinates.
   
-  let x = 310 * sin(frameCount * 0.05) + 250;
+  let x = 280 * sin(frameCount * 0.05) + 240;
   let y = 1400;
 
   // Draw the oscillator.
